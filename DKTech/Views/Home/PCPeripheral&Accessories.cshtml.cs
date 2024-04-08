5277bff13dfa68abf8b3ccd5b058169ea04faadd
Dexter Ku <ac116595@avcol.school.nz>
2024-04-08 12:50:10 +1200

    added new pages about us, contact us, etc. need to fix razor pages and add details.

---
+++ DKTech/Views/Home/PCPeripheral&Accessories.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DKTech.Views.Home
+{
+    public class PCPeripheral_AccessoriesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ DKTech/Views/Home/pc&laptops.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DKTech.Views.Home
+{
+    public class pc_laptopsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
