41fbcc552ab4b281dbedeb1c056071ad23b55d8c
Dexter Ku <ac116595@avcol.school.nz>
2024-08-08 15:15:14 +1200

    changed cart link from a dropdown to the actual Cart icon. Need to fix department dropdown for models pages.

---
+++ DKTech/Migrations/20240808025123_OrderDate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DKTech.Migrations
+{
+    /// <inheritdoc />
+    public partial class OrderDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Order_date",
+                table: "Customer",
+                newName: "OrderDate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "OrderDate",
+                table: "Customer",
+                newName: "Order_date");
+        }
+    }
+}
