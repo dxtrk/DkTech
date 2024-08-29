7192db1b1c014e89171855dcd1e01b63a596de17
Dexter Ku <ac116595@avcol.school.nz>
2024-08-29 15:12:55 +1200

    Working on the errors in Contact model.

--- DKTech/Models/Contact.cs
+++ DKTech/Models/Contact.cs
@@ -1,20 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using DKTech.Areas.Identity.Data;
+using System.ComponentModel.DataAnnotations;
 
 namespace DKTech.Models
 
-{ 
-public class Contact
 {
-    public int ContactID { get; set; }
+    public class Contact
+    {
+        public int ContactID { get; set; }
         // user ID from AspNetUser table.
         public string? OwnerID { get; set; }
         public string Name { get; set; }
-    public string Address { get; set; }
-    public string City { get; set; }
-    public string State { get; set; }
-    public string Zip { get; set; }
-    [DataType(DataType.EmailAddress)]
-    public string Email { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
         public ContactStatus Status { get; set; }
     }
 
@@ -24,25 +25,26 @@ public class Contact
         Approved,
         Rejected
     }
-    public static void SeedDB(ApplicationDbContext context, string adminID)
-{
-    if (context.Contact.Any())
+    public static void SeedData(DKTechContext context, string adminID)
     {
-        return;   // DB has been seeded
-    }
-
-    context.Contact.AddRange(
-        new Contact
+        if (context.Contact.Any())
         {
-            Name = "Debra Garcia",
-            Address = "1234 Main St",
-            City = "Redmond",
-            State = "WA",
-            Zip = "10999",
-            Email = "ac116595@avcol.school.nz",
-            Status = ContactStatus.Approved,
-            OwnerID = adminID
-        },
+            return;   // DB has been seeded
+        }
+
+        context.Contact.AddRange(
+            new Contact
+            {
+                Name = "Dexter Ku",
+                Address = "1234 Dale St",
+                City = "Auckland",
+                State = "Auckland",
+                Zip = "10999",
+                Email = "ac116595@avcol.school.nz",
+                Status = ContactStatus.Approved,
+                OwnerID = adminID
+            });
+    }
 }
 
 
